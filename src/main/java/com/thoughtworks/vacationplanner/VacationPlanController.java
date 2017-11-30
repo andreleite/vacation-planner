75bc41597dd5bcbfb91af6c27b6a4df0d939de7d
Andre Leite <aleite@thoughtworks.com>
2017-11-30 14:27:12 -0200

    return the next endpoint url

--- src/main/java/com/thoughtworks/vacationplanner/VacationPlanController.java
+++ src/main/java/com/thoughtworks/vacationplanner/VacationPlanController.java
@@ -13,20 +13,13 @@ public class VacationPlanController {
 
     private RestTemplate restTemplate = new RestTemplate();
 
-    @RequestMapping("/hello")
-    public String hello() {
-        City[] cities = requestCities();
-
-        return cities[0].getDistrict();
-    }
-
     @RequestMapping("/vacation-plan")
     public String vacationPlan(@RequestParam Map<String, String> queryParameters) {
-        return getCityIdFromName(queryParameters.get("city"));
+        String cityId = getCityIdFromName(queryParameters.get("city"));
+        return "http://localhost:8882/cities/" + cityId + "/year/2017";
     }
 
     private String getCityIdFromName(String city) {
-        
         return Arrays.stream(requestCities())
             .filter(c -> c.getDistrict().equals(city))
             .findFirst()
--- src/test/java/com/thoughtworks/vacationplanner/VacationPlanControllerTest.java
+++ src/test/java/com/thoughtworks/vacationplanner/VacationPlanControllerTest.java
@@ -9,16 +9,6 @@ import java.util.Map;
 import static org.junit.Assert.assertEquals;
 
 public class VacationPlanControllerTest {
-
-    @Test
-    public void hello() throws Exception {
-        VacationPlanController vacationPlanControllerTest = new VacationPlanController();
-
-        String hello = vacationPlanControllerTest.hello();
-
-        assertEquals("Porto Alegre", hello);
-    }
-
     @Test
     public void retrievesQueryParameter() throws Exception {
         VacationPlanController vacationPlanController = new VacationPlanController();
@@ -30,7 +20,7 @@ public class VacationPlanControllerTest {
 
         String response = vacationPlanController.vacationPlan(params);
 
-        assertEquals("455821", response);
+        assertEquals("http://localhost:8882/cities/455821/year/2017", response);
     }
 
 }
