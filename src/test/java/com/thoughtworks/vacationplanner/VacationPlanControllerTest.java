c64d0d05d361227b2191577e04a1098a10cb3cf0
Bruna Pereira <bpereira@thoughtworks.com>
2017-12-01 14:54:29 -0200

    adds failing test. let the competition begins

--- src/test/java/com/thoughtworks/vacationplanner/VacationPlanControllerTest.java
+++ src/test/java/com/thoughtworks/vacationplanner/VacationPlanControllerTest.java
@@ -9,17 +9,16 @@ import static org.junit.Assert.assertEquals;
 
 public class VacationPlanControllerTest {
     @Test
-    public void retrievesQueryParameter() throws Exception {
+    public void returnsAllPossibleRangeDays() throws Exception {
         VacationPlanController vacationPlanController = new VacationPlanController();
 
         Map<String, String> params = new HashMap<>();
         params.put("city", "Porto Alegre");
-        params.put("days", "10");
-        params.put("weather", "clear");
+        params.put("days", "15");
+        params.put("weather", "Clear, Partly Cloudy, Cold");
 
         String response = vacationPlanController.vacationPlan(params);
 
-        assertEquals("thunderstorms", response);
+        assertEquals("De 20 de Junho a 10 de Julho; De 09 de Maio a 26 de Maio; De 02 de Abril a 16 de Abril", response);
     }
-
 }
